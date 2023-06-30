1765bbbdfec8eb9af643044f06251dc0413fc06a
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-06-30 11:05:19 +0200

    feat:add lazy loading

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Routes as RouteList, Route } from "react-router-dom";
 
-import { Home, StreamerRecord } from "../pages";
+import { CircularProgress } from "@mui/material";
+
+const Home = lazy(() => import("../pages/Home"));
+const StreamerRecord = lazy(() => import("../pages/StreamerRecord"));
 
 import { NESTED_PATHS, PATHS } from "./paths";
 
 const Routes: React.FC = () => {
   return (
     <RouteList>
-      <Route path={PATHS.DEFAULT} element={<Home />} />
-      <Route path={NESTED_PATHS.STREAMER} element={<StreamerRecord />} />
+      <Suspense fallback={<CircularProgress />}>
+        <Route path={PATHS.DEFAULT} element={<Home />} />
+        <Route path={NESTED_PATHS.STREAMER} element={<StreamerRecord />} />
 
-      <Route path={PATHS.NOT_FOUND} element={<Home />} />
+        <Route path={PATHS.NOT_FOUND} element={<Home />} />
+      </Suspense>
     </RouteList>
   );
 };
